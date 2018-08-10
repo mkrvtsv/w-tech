da0ffbe5f82623fa2a6acd00af66398647b7ea16
Mike Kravtsov <mike.kravtsov@gmail.com>
2018-08-10 22:01:01 +0300

    019 rm default

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
-sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
