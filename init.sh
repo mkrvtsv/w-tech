c668037e1964f50062dc216db416ec75fdc2fa95
Mike Kravtsov <mike.kravtsov@gmail.com>
2018-08-10 22:40:01 +0300

    020 myvirthost

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/
+sudo ln -s /home/box/web/etc/myvirthost /etc/nginx/sites-enabled/myvirthost
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
