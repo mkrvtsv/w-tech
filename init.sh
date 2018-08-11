3cb462d58305195b44d78c19314605de44da8ad6
Mike Kravtsov <mike.kravtsov@gmail.com>
2018-08-11 11:01:44 +0300

    025 init

--- init.sh
+++ init.sh
@@ -1,4 +1,2 @@
-sudo rm /etc/nginx/sites-enabled/test.conf
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo rm /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart 
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
