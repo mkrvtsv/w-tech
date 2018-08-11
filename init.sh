ee56083439629b181b7932ec31a3821f2e01a0a4
Mike Kravtsov <mike.kravtsov@gmail.com>
2018-08-11 10:57:25 +0300

    025 init

--- init.sh
+++ init.sh
@@ -1,10 +1,4 @@
 sudo rm /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-
-sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
-
-sudo /etc/init.d/mysql restart
-mysql -u root -e "CREATE DATABASE qa CHARACTER SET utf8"
+sudo /etc/init.d/nginx restart 
