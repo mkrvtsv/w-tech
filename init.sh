83f9ebffc2d12d9fb6514de1bb79163ad22cd157
Mike Kravtsov <mike.kravtsov@gmail.com>
2018-08-20 18:13:49 +0300

    02_007 init, gunicorn.conf -> gunicorn.py

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/
 sudo unlink /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/
+sudo ln -s /home/box/web/etc/gunicorn.py /etc/gunicorn.d/gunicorn.py
 sudo /etc/init.d/gunicorn restart﻿﻿
