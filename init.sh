c1fd7671c5345f0e087f2d1bf735b3f8ed2f4f6a
Mike Kravtsov <mike.kravtsov@gmail.com>
2018-08-20 17:59:45 +0300

    02_006 init, backend -> gunicorn

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/
 sudo unlink /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/hello.py /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/
 sudo /etc/init.d/gunicorn restart﻿﻿
